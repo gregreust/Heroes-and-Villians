f3cfa7438a0bf868a0d3083b72a0042ff8d2d69a
gregreust <greg.reust@gmail.com>
2022-08-23 10:05:03 -0500

    Created SuperTypes app and model + created supers app and model

--- heroes_and_villians_project/settings.py
+++ heroes_and_villians_project/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)!)1u2aw_7o(&^3=o3@%8qwi=@%2-i=z#&$f!+*=kpcmppyx#c'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -70,15 +69,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'heroes_and_villians_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -121,3 +111,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from heroes_and_villians_project.local_settings import *
+except ImportError:
+    pass
---
+++ supers/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from super_types import SuperType
+
+# Create your models here.
+class Supers(models.Model):
+    name = models.CharField(max_length=60)
+    alter_ego = models.CharField(max_length=60)
+    primary_ability = models.CharField(max_length=60)
+    secondary_ability = models.CharField(max_length=60)
+    catchphrase = models.CharField(max_length=150)
+    super_type =models.ForeignKey(SuperType, on_delete=models.SET_NULL)
+
+    
